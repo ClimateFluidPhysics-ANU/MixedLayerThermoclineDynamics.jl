7681046d26b7e47c95b56c64fd0582be72dc830f
Dhruv Bhagtani <dhruvbhagtani@gmail.com>
2021-07-06 09:56:50 +0530

    Including grids.jl

--- src/MixedLayerThermoclineDynamics.jl
+++ src/MixedLayerThermoclineDynamics.jl
@@ -2,25 +2,7 @@ module MixedLayerThermoclineDynamics
 
 export hello, domath
 
-"""
-    hello(who::String)
+include("grids.jl")
 
-Return "Hello, `who`".
-"""
-hello(who::String) = "Hello, $who"
-
-"""
-    domath(x::Number)
-
-Return `x + 5`.
-"""
-domath(x::Number) = x + 5
-
-"""
-    compute_square(x)
-
-Compute the square of something.
-"""
-compute_square(x) = x^2
 
 end # module
