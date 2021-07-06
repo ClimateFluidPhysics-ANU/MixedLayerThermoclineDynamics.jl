4b520e9553c2248c3accb9dca75a11fc498d6df1
Dhruv Bhagtani <dhruvbhagtani@gmail.com>
2021-07-06 09:56:19 +0530

    Grids for 1D and 2D case

---
+++ src/grids.jl
@@ -0,0 +1,39 @@
+struct Grid1D
+    nx::Int
+    dx::Float64
+    Lx::Float64
+    xu::Vector
+    xt::Vector
+end
+
+function Grid1D(nx, x_beg, x_end)
+    dx = (x_end - x_beg)/(nx + 0.5)
+    xu = LinRange(x_beg, x_end - dx/2, nx)
+    xt = LinRange(x_beg + dx/2, x_end, nx)
+
+    return Grid1D(nx, dx, Lx, xu, xt)
+end
+
+struct Grid2D
+    nx::Int
+    ny::Int
+    dx::Float64
+    dy::Float64
+    Lx::Float64
+    Ly::Float64
+    xu::Vector
+    xt::Vector
+    yu::Vector
+    yt::Vector
+end
+
+function Grid2D(nx, ny, x_beg, x_end, y_beg, y_end)
+    dx = (x_end - x_beg)/(nx + 0.5)
+    dy = (y_end - y_beg)/(ny + 0.5)
+    xu = LinRange(x_beg, x_end - dx/2, nx)
+    xt = LinRange(x_beg + dx/2, x_end, nx)
+    yu = LinRange(y_beg, y_end - dy/2, ny)
+    yt = LinRange(y_beg + dy/2, y_end, ny)
+    
+    return Grid2D(nx, ny, dx, dy, Lx, Ly, xu, xt, yu, yt)
+end
