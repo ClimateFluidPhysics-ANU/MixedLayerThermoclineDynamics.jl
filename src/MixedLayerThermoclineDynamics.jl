bdd29223197aff6ce6246618cfa9bdb6b9d284ce
Dhruv Bhagtani <dhruvbhagtani@gmail.com>
2021-07-12 23:39:37 +0530

    Exporting Field1D and Field2D for easy access

--- src/MixedLayerThermoclineDynamics.jl
+++ src/MixedLayerThermoclineDynamics.jl
@@ -3,10 +3,12 @@ module MixedLayerThermoclineDynamics
 using DocStringExtensions
 
 export Grid1D, Grid2D
+export Field1D, Field2D
 
 "Abstract supertype for grids."
 abstract type AbstractGrid end
 
 include("grids.jl")
+include("fields.jl")
 
 end # module
