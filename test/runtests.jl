3a873f674ef962bafd4f35d527e36099cda73085
Dhruv Bhagtani <dhruvbhagtani@gmail.com>
2021-07-12 16:14:13 +0530

    Added more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,18 @@ using Test, MixedLayerThermoclineDynamics
     Lx, Ly = 2.0, 2.4
     
     grid1D = Grid1D(nx, 0, Lx)
-    
+
     @test test_dx(grid1D, Lx/nx)
+    @test test_xF(grid1D, LinRange(0, Lx, nx))
+
+    grid2D = Grid2D(nx, ny, 0, Lx, 0, Ly)
+
+    @test test_dx(grid2D, Lx/nx)
+    @test test_dy(grid2D, Ly/ny)
+    @test test_xF(grid2D, LinRange(dx/2, Lx - dy/2, nx))
+    @test test_xC(grid2D, LinRange(0, Lx - dx, nx))
+    @test test_xF(grid2D, LinRange(0, Ly - dy, ny))
+    @test test_yC(grid2D, LinRange(dy/2, Lx - dy/2, ny))
+    @test xdomain_length(grid2D, Lx - 0)
+    @test ydomain_length(grid2D, Ly - 0)
 end
--- test/test_grids.jl
+++ test/test_grids.jl
@@ -1,3 +1,31 @@
 function test_dx(grid, dx)
     return grid.dx ≈ dx
 end
+
+function dxdy(grid, dx, dy)
+    return grid.dy ≈ dy
+end
+
+function test_xF(grid, xF)
+    return grid.xF ≈ xF
+end
+
+function test_xC(grid, xC)
+    return grid.xC ≈ xC
+end
+
+function test_yF(grid, yF)
+    return grid.yF ≈ yF
+end
+
+function test_yC(grid, yC)
+    return grid.yC ≈ yC
+end
+
+function xdomain_length(grid, Lx)
+    return grid.Lx ≈ Lx
+end
+
+function ydomain_length(grid, Ly)
+    return grid.Ly ≈ Ly
+end
