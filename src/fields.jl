6d9853cde020a4bac8ce2f9a87b5951e844956b7
Dhruv Bhagtani <dhruvbhagtani@gmail.com>
2021-07-21 16:44:22 +0530

    Included grids as parameter in fields;

--- src/fields.jl
+++ src/fields.jl
@@ -14,16 +14,13 @@ A field datatype for 1D objects.
 
 $(TYPEDFIELDS)
 """
-struct Field1D{LX<:AbstractLocation} <: AbstractField
+struct Field1D{LX<:AbstractLocation, G} <: AbstractField
     "Array with the values of the field."
-    data
+    data :: Array
     "The grid on which the field lives."
-    grid :: Grid1D   
-end
+    grid :: G
 
-function Field1D(LX, data, grid)
-
-    return Field1D{LX}(fill_halos(data, grid, LX), grid)
+    Field1D(LX, data, grid::G) where G = new{LX, G}(data, grid)
 end
 
 """
@@ -33,19 +30,14 @@ A field datatype for 2D objects.
 
 $(TYPEDFIELDS)
 """
-struct Field2D{LX<:AbstractLocation, LY<:AbstractLocation} <: AbstractField
+struct Field2D{LX<:AbstractLocation, LY<:AbstractLocation, G} <: AbstractField
     "Array with the values of the field."
     data :: Array
     "The grid on which the field lives."
-    grid :: Grid2D
+    grid :: G
     
-    Field2D(LX, LY, data, grid) = new{LX, LY}(data, grid)    
+    Field2D(LX, LY, data, grid::G) where G = new{LX, LY, G}(data, grid)    
 end
-
-#function Field2D(LX, LY, data, grid)
-#    C = fill_halos(data, grid)
-#    return Field2D{LX, LY}(C, grid)
-#end
 
 """
     Field(LX, data, grid::Grid1D)
@@ -61,57 +53,28 @@ Constructs a 2D field of `data` at location `(LX, LY)` on `grid`.
 """
 Field(LX, LY, data, grid::Grid2D) = Field2D(LX, LY, data, grid)
 
-function fill_halos(data, grid::Grid1D{Periodic}, LX::Type{Centre})
-    nx, hx, dx = grid.nx, grid.hx, grid.dx
-    offsetData = zero(grid.xC)
-    
-    for j in 1:hx
-        offsetData[nx+j] = data[j]
-        offsetData[-j+1] = data[nx-j+1]
-    end
-
-    for i in 1:nx
-        offsetData[i] = data[i]
-    end
-    return offsetData
-end
-
-function fill_halos(data, grid::Grid1D{Periodic}, LX::Type{Face})
-    nx, hx, dx = grid.nx, grid.hx, grid.dx
-    offsetData = zero(grid.xF)
-
-    for j in 1:hx
-        offsetData[nx+j] = data[j]
-        offsetData[-j+1] = data[nx-j+1]
-    end
-
-    for i in 1:nx
-        offsetData[i] = data[i]
-    end
-    return offsetData
-end
-
 ############################################################################
 #-------------------------------Interpolation------------------------------#
 
-𝐼xᶠ(i, f::Field1D{Face})   = (f.data[i] + f.data[i+1]) / 2
-𝐼xᶜ(i, f::Field1D{Centre}) = (f.data[i] + f.data[i-1]) / 2
+𝐼xᶠ(i::Int, f::Field1D{Face})   = (f.data[i] + f.data[i+1]) / 2
+𝐼xᶜ(i::Int, f::Field1D{Centre}) = (f.data[i] + f.data[i-1]) / 2
 
-𝐼xᶠ(i, j, f::Field2D{Face, Centre})   = (f.data[i, j] + f.data[i+1, j]) / 2
-𝐼xᶜ(i, j, f::Field2D{Centre, Centre}) = (f.data[i, j] + f.data[i-1, j]) / 2
-𝐼yᶠ(i, j, f::Field2D{Centre, Face})   = (f.data[i, j+1] + f.data[i, j]) / 2
-𝐼yᶜ(i, j, f::Field2D{Centre, Centre}) = (f.data[i, j] + f.data[i, j-1]) / 2
+𝐼xᶠ(i::Int, j::Int, f::Field2D{Face, Centre})   = (f.data[i, j] + f.data[i+1, j]) / 2
+𝐼xᶜ(i::Int, j::Int, f::Field2D{Centre, Centre}) = (f.data[i, j] + f.data[i-1, j]) / 2
+𝐼yᶠ(i::Int, j::Int, f::Field2D{Centre, Face})   = (f.data[i, j+1] + f.data[i, j]) / 2
+𝐼yᶜ(i::Int, j::Int, f::Field2D{Centre, Centre}) = (f.data[i, j] + f.data[i, j-1]) / 2
 
 """
     𝐼x!(output::Field1D{Centre}, input::Field1D{Face})
 
 Interpolates a 1D field of `data` from Face to Centre.
 """
-function 𝐼x!(output::Field1D{Centre}, input::Field1D{Face})
+function 𝐼x!(output::Field1D{Centre}, input::Field1D{Face, Grid1D{Periodic}})
     nx = input.grid.nx
         
-    for i in 1:nx
-        output.data[i] = 𝐼xᶠ(i, input.data)
+    output.data[nx] = (input.data[1] + input.data[nx]) / 2
+    for i in 1:nx-1
+        output.data[i] = 𝐼xᶠ(i, input)
     end
 end
 
@@ -120,70 +83,81 @@ end
 
 Interpolates a 1D field of `data` from Centre to Face.
 """
-function 𝐼x!(output::Field1D{Face}, input::Field1D{Centre})
+function 𝐼x!(output::Field1D{Face}, input::Field1D{Centre, Grid1D{Periodic}})
     nx = input.grid.nx
         
-    for i in 1:nx+1
-        output.data[i] = 𝐼xᶜ(i, input.data)
+    output.data[1] = (input.data[1] + input.data[nx]) / 2
+    for i in 2:nx
+        output.data[i] = 𝐼xᶜ(i, input)
     end
 end
 
 """
-    𝐼x!(output::Field2D{Centre}, input::Field2D{Face})
+    𝐼x!(output::Field2D{Face, Centre}, input::Field2D{Centre, Centre})
 
 Interpolates a 2D field of `data` from T to U grid.
 """
-function 𝐼x!(output::Field2D{Face, Centre}, input::Field2D{Centre, Centre})
+function 𝐼x!(output::Field2D{Face, Centre}, input::Field2D{Centre, Centre, Grid2D{Periodic, Periodic}})
     nx, ny = input.grid.nx, input.grid.ny
     
-    for i in 1:nx+1
+    for j in 1:ny
+        output.data[1, j] = (input.data[1, j] + input.data[nx, j]) / 2
+    end
+
+    for i in 2:nx
         for j in 1:ny
-            output.data[i, j] = 𝐼xᶠ(i, j, input.data)
+            output.data[i, j] = 𝐼xᶜ(i, j, input)
         end
     end
 end
 
 """
-    𝐼x!(output::Field2D{Face}, input::Field2D{Centre})
+    𝐼x!(output::Field2D{Centre, Centre}, input::Field2D{Face, Centre})
 
 Interpolates a 2D field of `data` from U to T grid.
 """
-function 𝐼x!(output::Field2D{Centre, Centre}, input::Field2D{Face, Centre})
+function 𝐼x!(output::Field2D{Centre, Centre}, input::Field2D{Face, Centre, Grid2D{Periodic, Periodic}})
     nx, ny = input.grid.nx, input.grid.ny
         
-    for i in 1:nx
+    for j in 1:ny
+        output.data[nx, j] = (input.data[1, j] + input.data[nx, j]) / 2
+    end
+
+    for i in 1:nx-1
         for j in 1:ny
-            output.data[i, j] = 𝐼xᶜ(i, j, input.data)
+            output.data[i, j] = 𝐼xᶠ(i, j, input)
         end
     end
 end
 
 """
-    𝐼y!(output::Field1D{Face}, input::Field2D{Centre})
+    𝐼y!(output::Field1D{Centre, Face}, input::Field2D{Centre, Centre})
 
 Interpolates a 2D field of `data` from T to V grid.
 """
-function 𝐼y!(output::Field2D{Centre, Face}, input::Field2D{Centre, Centre})
+function 𝐼y!(output::Field2D{Centre, Face}, input::Field2D{Centre, Centre, Grid2D{Periodic, Periodic}})
     nx, ny = input.grid.nx, input.grid.ny
         
     for i in 1:nx
-        for j in 1:ny+1
-            output.data[i, j] = 𝐼yᶠ(i, j, input.data)
+            output.data[i, 1] = (input.data[i, 1] + input.data[i, ny]) / 2
+        for j in 2:ny
+            output.data[i, j] = 𝐼yᶜ(i, j, input)
         end
     end
 end
 
 """
-    𝐼y!(output::Field2D{Face}, input::Field2D{Centre})
+    𝐼y!(output::Field2D{Centre, Centre}, input::Field2D{Centre, Face})
 
 Interpolates a 2D field of `data` from V to T grid.
 """
-function 𝐼y!(output::Field2D{Centre, Centre}, input::Field2D{Centre, Face})
+function 𝐼y!(output::Field2D{Centre, Centre}, input::Field2D{Centre, Face, Grid2D{Periodic, Periodic}})
     nx, ny = input.grid.nx, input.grid.ny
         
     for i in 1:nx
-        for j in 1:ny
-            output.data[i, j] = 𝐼yᶜ(i, j, input.data)
+        output.data[i, ny] = (input.data[i, 1] + input.data[i, ny]) / 2
+        for j in 1:ny-1
+            output.data[i, j] = 𝐼yᶠ(i, j, input)
         end
     end
 end
@@ -209,7 +183,7 @@ function ∂x!(output::Field1D{Centre}, input::Field1D{Face})
     dx = input.grid.dx
         
     for i in 1:nx
-        output.data[i] = δxᶠ(i, input.data)/dx
+        output.data[i] = δxᶠ(i, input)/dx
     end
 end
 
@@ -223,7 +197,7 @@ function ∂x!(output::Field1D{Face}, input::Field1D{Centre})
     dx = input.grid.dx
 
     for i in 1:nx+1
-        output.data[i] = δxᶜ(i, input.data)/dx
+        output.data[i] = δxᶜ(i, input)/dx
     end
 end
 
@@ -238,7 +212,7 @@ function ∂x!(output::Field2D{Centre, Centre}, input::Field2D{Face, Centre})
         
     for i in 1:nx
         for j in 1:ny
-            output.data[i, j] = δxᶠ(i, j, input.data)/dx
+            output.data[i, j] = δxᶠ(i, j, input)/dx
         end
     end
 end
@@ -254,7 +228,7 @@ function ∂x!(output::Field2D{Face, Centre}, input::Field2D{Centre, Centre})
 
     for i in 1:nx+1
         for j in 1:ny
-            output.data[i, j] = δxᶜ(i, j, input.data)/dx
+            output.data[i, j] = δxᶜ(i, j, input)/dx
         end
     end
 end
@@ -270,7 +244,7 @@ function ∂y!(output::Field2D{Centre, Centre}, input::Field2D{Centre, Face})
         
     for i in 1:nx
         for j in 1:ny
-            output.data[i, j] = δyᶠ(i, j, input.data)/dy
+            output.data[i, j] = δyᶠ(i, j, input)/dy
         end
     end
 end
@@ -286,7 +260,7 @@ function ∂y!(output::Field2D{Centre, Face}, input::Field2D{Centre, Centre})
 
     for i in 1:nx
         for j in 1:ny+1
-            output.data[i, j] = δyᶜ(i, j, input.data)/dy
+            output.data[i, j] = δyᶜ(i, j, input)/dy
         end
     end
 end
